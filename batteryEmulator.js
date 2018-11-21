5dc7caaeba31583df630707eae9c680e9b425f18
Zakary Timson <zaktimson@gmail.com>
2018-11-21 13:02:58 -0500

    Update batteryEmulator.js

--- batteryEmulator.js
+++ batteryEmulator.js
@@ -1,41 +1,40 @@
-const axios = require('axios');
-const fs = require('fs');
+const SerialPort = require('serialport');
 const firebase = require('firebase');
 
-const namespace = 'TEMP';
-const name = 'RaspberryPi';
+const namespace = '170614D';
+
+firebase.initializeApp({
+	apiKey: "AIzaSyAs3FvBCADM66wR1-leBz6aIjK1wZfUxRo",
+	authDomain: "homefront-2ccb4.firebaseapp.com",
+	databaseURL: "https://homefront-2ccb4.firebaseio.com",
+	projectId: "homefront-2ccb4",
+	storageBucket: "homefront-2ccb4.appspot.com",
+	messagingSenderId: "482384317544"
+});
 
 (async () => {
-	// Get temerature from sensor
-	let text = fs.readFileSync('/sys/bus/w1/devices/28-0000062c1c39/w1_slave');
-	let temp = /t\=(\d{5})/g.exec(text);
-	let cel = Number(temp[1])/1000;
-	console.log('temperture: ', cel);
-
-	// Send data to firebase
-	firebase.initializeApp({
-		apiKey: "AIzaSyAs3FvBCADM66wR1-leBz6aIjK1wZfUxRo",
-		authDomain: "homefront-2ccb4.firebaseapp.com",
-		databaseURL: "https://homefront-2ccb4.firebaseio.com",
-		projectId: "homefront-2ccb4",
-		storageBucket: "homefront-2ccb4.appspot.com",
-		messagingSenderId: "482384317544"
-	});
+	const ReadLine = SerialPort.parsers.Readline;
+	const parser = new SerailPort('/dev/ttyUSB0').pipe(new Readline());
+	
 	let firestore = firebase.firestore();
 	firestore.settings({timestampsInSnapshots: true})
 	let data = (await firestore.collection('Battery').doc(namespace).get()).data();
-	if(!data[name]) data[name] = [];
-	data[name].push({
-		temp: cel,
-		charging: false,
-		percentage: 0.5
+	
+	parser.on('data', dataIn => {
+		let data = dataIn.match(/\d+\.?\d*/g);
+		let moduleCount = 0;
+		data = data.reduce((acc, val, i, arr) => {
+			if(i % 2 == 1) return acc;
+			moduleCount++;
+			if(!acc[`Module ${moduleCount}`]) acc[`Module ${moduleCount}`] = [];
+			acc[`Module ${moduleCount}`].push({charge: val, temp: arr[i + 1]});
+		}, data);
+		
+		try {
+			await firestore.collection('Battery').doc(namespace).update(data)
+			process.exit();
+		} catch(err) {
+			process.exit(1);
+		}
 	});
-	data[name].splice(0, data.length - 1440);
-
-	try {
-		await firestore.collection('Battery').doc(namespace).update(data)
-		process.exit();
-	} catch(err) {
-		process.exit(1);
-	}
 })();
