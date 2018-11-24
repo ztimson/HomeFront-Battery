6095d2d0370b8160f445f59c3eb6ccb28be3e7a9
ztimson <zaktimson@gmail.com>
2018-11-23 21:27:06 -0500

    Removed test file

--- test.js
+++
@@ -1,6 +0,0 @@
-const SerialPort = require('serialport');
-const Readline = SerialPort.parsers.Readline;
-const port = new SerialPort('/dev/ttyUSB0');
-const parser = port.pipe(new Readline());
-parser.on('data', console.log);
-
