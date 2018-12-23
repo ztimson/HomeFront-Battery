5e509c1eca3123946ec249ce653dbf6b0d8ac41a
Zakary Timson <zaktimson@gmail.com>
2018-12-23 14:09:06 -0500

    Organized the file better

--- monitor.js
+++ monitor.js
@@ -17,51 +17,48 @@ firebase.initializeApp({
 const firestore = firebase.firestore();
 firestore.settings({timestampsInSnapshots: true});
 
-(async () => {
-	// Get document and data
+var modules; // Where the latest data is stored
+serialParser.on('data', async serialIn => {
+	let sensorData = serialIn.match(/\d+\.?\d*/g);
+
+	// Divide data into modules
+	let moduleCount = 0;
+	modules = sensorData.reduce((acc, val, i, arr) => {
+		if(i % 2 == 1) return acc;
+
+		// Construct data set
+		moduleCount++;
+		let name = `Module ${moduleCount}`;
+		acc[name] = {charge: Number(val), temp: Number(arr[i + 1])};
+		return acc;
+	}, {});
+});
+
+setInterval(async () => {
+	if(!modules) return;
+
 	let doc = await firestore.collection('Battery').doc(namespace).get();
-	let data = doc.data() || {};
-	const config = data.config || {};
-	
-	let modules;
-	serialParser.on('data', async serialIn => {
-		let sensorData = serialIn.match(/\d+\.?\d*/g);
+	let data = doc.data() || {config: {}, modules: {}};
+	const config = data.config;
 
-		// Divide data into modules
-		let moduleCount = 0;
-		modules = sensorData.reduce((acc, val, i, arr) => {
-			if(i % 2 == 1) return acc;
-
-			// Construct data set
-			moduleCount++;
-			let name = `Module ${moduleCount}`;
-			acc[name] = {charge: Number(val), temp: Number(arr[i + 1])};
-			return acc;
-		}, {});
+	// Add latest data
+	Object.keys(modules).forEach(key => {
+		if(!data.modules[key]) data.modules[key] = [];
+		data.modules[key].push(modules[key]);
+		data.modules[key].splice(0, data.modules[key].length - 1440);
 	});
-	
-	setInterval(() => {
-		if(!modules) return;
-		
-		// Add latest data
-		Object.keys(modules).forEach(key => {
-			if(!data.modules[key]) data.modules[key] = [];
-			data.modules[key].push(modules[key]);
-			data.modules[key].splice(0, data.modules[key].length - 1440);
-		});
-		
-		// Turn the relay on/off
-		/* if(config.relayMode != null) {
-			console.log(config.relayMode, Number(config.relayMode));
-			port.write(Number(config.relayMode).toString());
-		} else {
-			let turnOn = sensorData.filter((ignore, i) => i % 2 == 1).filter(temp => temp >= (config.relayOn || 50)).length > 0;
-			let turnOff = sensorData.filter((ignore, i) => i % 2 == 1).filter(temp => temp <= (config.relayOff || 35)).length > 0;
-			if(turnOn) port.write([1]);
-			if(turnOff && !turnOn) port.write([0]);
-		}*/
-		
-		// Submit
-		doc.ref[doc.exists ? 'update' : 'set'](data);
-	}, 60000);
-})();
+
+	// Turn the relay on/off
+	/* if(config.relayMode != null) {
+		console.log(config.relayMode, Number(config.relayMode));
+		port.write(Number(config.relayMode).toString());
+	} else {
+		let turnOn = sensorData.filter((ignore, i) => i % 2 == 1).filter(temp => temp >= (config.relayOn || 50)).length > 0;
+		let turnOff = sensorData.filter((ignore, i) => i % 2 == 1).filter(temp => temp <= (config.relayOff || 35)).length > 0;
+		if(turnOn) port.write([1]);
+		if(turnOff && !turnOn) port.write([0]);
+	}*/
+
+	// Submit
+	doc.ref[doc.exists ? 'update' : 'set'](data);
+}, 60000);
