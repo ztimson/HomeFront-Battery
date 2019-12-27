a8a0125ed8640070a6d0766f4155feba08197aab
ztimson <zaktimson@gmail.com>
2019-12-26 23:14:07 -0500

    Updated to work with BMS instead of arduino

--- monitor.js
+++ monitor.js
@@ -1,10 +1,8 @@
-const fs = require('fs');
-const SerialPort = require('serialport');
+const bms = require('./bms');
 const firebase = require('firebase');
+const namespace = require('fs').readFileSync('/etc/hostname', 'utf8').trim();
 
-const namespace = fs.readFileSync('/etc/hostname', 'utf8').trim();
-const port = new SerialPort('/dev/ttyUSB0');
-
+// init
 firebase.initializeApp({
 	apiKey: "AIzaSyAs3FvBCADM66wR1-leBz6aIjK1wZfUxRo",
 	authDomain: "homefront-2ccb4.firebaseapp.com",
@@ -15,51 +13,15 @@ firebase.initializeApp({
 });
 const firestore = firebase.firestore();
 firestore.settings({timestampsInSnapshots: true});
+const powerwall = new bms('/dev/ttyACM0');
+const doc = firestore.collection('Battery').doc(namespace).collection('data').doc(new Date().toString());
 
-function getData() {
-	return new Promise(res => {
-		let serialParser = port.pipe(new SerialPort.parsers.Readline());
-		serialParser.on('data', serialIn => {
-			// Format data
-			console.log(`(${(new Date()).toISOString()}) Input: ${serialIn}`);
-			let sensorData = serialIn.match(/\d+\.?\d*/g);
-			if(sensorData.length % 2 == 1) return;
-
-			// Split data & find averages
-			let chargeData = sensorData.filter((v, i) => i % 2 == 0);
-			let tempData = sensorData.filter((v, i) => i % 2 == 1);
-			let chargeAvg = chargeData.reduce((acc, val) => acc + val, 0) / chargeData.length;
-			let tempAvg = tempData.reduce((acc, val) => acc + val, 0) / tempData.length;
-
-			// Check data for signs of corruption
-			if(Math.sqrt(chargeData.reduce((acc, val) => acc + (val - chargeAvg) ** 2, 0) / (chargeData.length - 1)) > 3) return;
-			if(Math.sqrt(tempData.reduce((acc, val) => acc + (val - tempAvg) ** 2, 0) / (tempData.length - 1)) > 3) return;
-
-			// Cleanup
-			serialParser.removeAllListeners();
-			port.close();
-
-			// Submit the data
-			let data = {
-				timestamp: new Date(),
-				payload: chargeData.map((charge, i) => ({charge: Number(charge), temp: Number(tempData[i])}))
-			};
-			console.log(`(${(new Date()).toISOString()}) Output: ${JSON.stringify(data)}`);
-			res(data);
-		});
-	});
-}
-
-(async() => {
-	// Init
-	let data = await getData();
-	let doc = await firestore.collection('Battery').doc(namespace).collection('data').doc(data.timestamp.getTime().toString());
-
-	// Add latest data
+// Wait to accumulate data and then submit
+setTimeout(async () => {
 	console.log(`(${(new Date()).toISOString()}) Saving...`);
-	await doc.set(data);
+	await doc.set(powerwall.data());
+	powerwall.close();
 	console.log(`(${(new Date()).toISOString()}) Saved`);
-
-	// Submit
 	process.exit();
-})();
+}, 5000);
+
